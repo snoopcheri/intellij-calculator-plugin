87c4ca48b7f7cbaf9ab8123aebf4ee24b7e08a00
GitHub Action <action@github.com>
2022-02-25 10:03:37 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/snoopcheri/intellijcalculatorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.snoopcheri.intellijcalculatorplugin.services
+
+import com.github.snoopcheri.intellijcalculatorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/snoopcheri/intellijcalculatorplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/snoopcheri/intellijcalculatorplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.snoopcheri.intellijcalculatorplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.snoopcheri.intellijcalculatorplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
